7a99e074684d460bf80cc5ce0cf17eede931067c
MerlinEgalite <44097430+MerlinEgalite@users.noreply.github.com>
2022-06-12 23:35:49 +0200

    doc: add Natspec doc

--- src/MorphoToken.sol
+++ src/MorphoToken.sol
@@ -3,6 +3,13 @@ pragma solidity ^0.8.13;
 
 import "lib/mangrovedao/semitransferable-token/src/Token.sol";
 
+/// @title MorphoToken.
+/// @author Morpho Association.
+/// @custom:contact security@morpho.xyz
+/// @notice Morpho's token contract.
 contract MorphoToken is Token {
-	constructor(address _owner) Token("Morpho Token", "MORPHO", 18, _owner) {}
+
+  /// @notice Constructs Morpho's token contract.
+  /// @param _owner The address of the owner (Morpho DAO).
+  constructor(address _owner) Token("Morpho Token", "MORPHO", 18, _owner) {}
 }
