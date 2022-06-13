1c8cbd1acdedc59f5d1dfc3ee51de0d477bffc16
MerlinEgalite <44097430+MerlinEgalite@users.noreply.github.com>
2022-06-13 08:43:00 +0200

    test: add test draft

---
+++ test/TestMorphoToken.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+contract TestMorphoToken is Test {
+
+    function setUp() public {}
+
+    function testNonTransferable() public {}
+    function testWhitelisting() public {}
+    function testFullyTransferable() public {}
+}
