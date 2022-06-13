63ce2ab44ae28f899837096e6bf13d9b7002a26a
MerlinEgalite <egalite.merlin@gmail.com>
2022-06-13 15:41:09 +0200

    refactor: prettified

--- script/Deploy.sol
+++ script/Deploy.sol
@@ -5,18 +5,26 @@ import "src/MorphoToken.sol";
 import "forge-std/Script.sol";
 
 contract Deploy is Script {
-  address constant owner = address(0x0);
+    address constant owner = address(0x0);
 
-  function run() external {
-    vm.startBroadcast();
-    MorphoToken token = new MorphoToken(
-      msg.sender
-    );
-    token.setRoleCapability(0,address(token),Token.transfer.selector,true);
-    token.setRoleCapability(0,address(token),Token.transferFrom.selector,true);
-    token.setRoleCapability(1,address(token),Token.mint.selector,true);
+    function run() external {
+        vm.startBroadcast();
+        MorphoToken token = new MorphoToken(msg.sender);
+        token.setRoleCapability(
+            0,
+            address(token),
+            Token.transfer.selector,
+            true
+        );
+        token.setRoleCapability(
+            0,
+            address(token),
+            Token.transferFrom.selector,
+            true
+        );
+        token.setRoleCapability(1, address(token), Token.mint.selector, true);
 
-    token.setOwner(owner);
-    vm.stopBroadcast();
-  }
+        token.setOwner(owner);
+        vm.stopBroadcast();
+    }
 }
